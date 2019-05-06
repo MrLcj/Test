c2fd0343fd83afa81ad1f482c3b5feb9891e9224
liaochangjie <617190078@qq.com>
2019-05-06 16:24:18 +0800

    one

---
+++ test-cople/src/main/java/com/eicg/cople/TestCopleApplication.java
@@ -0,0 +1,13 @@
+package com.eicg.cople;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCopleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCopleApplication.class, args);
+	}
+
+}
---
+++ test-master/src/test/java/cn/eicg/test/TestMasterApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.eicg.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestMasterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
